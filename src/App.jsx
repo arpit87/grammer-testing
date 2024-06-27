b38bc835e45a8d5d6213791b2d61d531eb2d816b
nkraj7266 <nkraj7266@gmail.com>
2024-06-27 11:33:25 +0530

    fixed minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Oval } from "react-loader-spinner";
 
 function App() {
 	const [text, setText] = useState("");
+	const [fetched, setFetched] = useState(false);
 	const [trinka, setTrinka] = useState({});
 	const [textgears, setTextgears] = useState({});
 	const [languagetool, setLanguagetool] = useState({});
@@ -14,6 +15,7 @@ function App() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		setLoading(true);
+		setFetched(false);
 		console.log(text);
 		try {
 			const res = await axios.post(
@@ -26,6 +28,7 @@ function App() {
 			setTextgears(res.data.textgears);
 			setLanguagetool(res.data.languagetool);
 			setGramformer(res.data.gramformer);
+			setFetched(true);
 		} catch (error) {
 			console.log(error);
 		} finally {
@@ -58,205 +61,234 @@ function App() {
 				</div>
 			</form>
 			<br />
-			<div className="results">
-				{trinka?.status && (
-					<div>
-						<h2>Results (Trinka API)</h2>
-						<div className="errors">
-							{trinka.response.length === 0 && (
-								<p>No errors found!</p>
-							)}
-							{trinka.response.map((error, index) => (
-								<div
-									key={index}
-									className={index % 2 === 0 ? "even" : "odd"}
-								>
-									<p>
-										<strong>Sentence {index + 1}: </strong>
-										{error.sentence}
-									</p>
-									{error.sentence_result.map(
-										(result, idx) => (
-											<div
-												key={idx}
-												style={{
-													paddingLeft: "15px",
-												}}
-											>
-												<p>
-													<strong>
-														Wrong Part {idx + 1}:{" "}
-													</strong>
-													{result.covered_text}
-												</p>
-												{result.output.map(
-													(output, i) => (
-														<div
-															key={i}
-															style={{
-																paddingLeft:
-																	"15px",
-															}}
-														>
-															<p>
-																<strong>
-																	Suggested
-																	Corrections{" "}
-																	{i + 1}:{" "}
-																</strong>
-																{
-																	output.revised_text
-																}
-															</p>
-															<p>
-																<strong>
-																	Comment:{" "}
-																</strong>
-																{output.comment}
-															</p>
-														</div>
-													)
-												)}
-											</div>
-										)
-									)}
-								</div>
-							))}
-						</div>
-					</div>
-				)}
-				{textgears?.status && (
-					<div>
-						<h2>Results (TextGears API)</h2>
-						<div className="errors">
-							{textgears.response.errors.length === 0 && (
-								<p>No errors found!</p>
-							)}
-							{textgears.response.errors.map((error, index) => (
-								<div
-									key={error.id}
-									className={index % 2 === 0 ? "even" : "odd"}
-								>
-									<p>
-										<strong>Error {index + 1}: </strong>
-										{error.bad}
-									</p>
-									<div>
+			{fetched && (
+				<div className="results">
+					{trinka?.status && (
+						<div>
+							<h2>Results (Trinka API)</h2>
+							<div className="errors">
+								{trinka.response.length === 0 && (
+									<p>No errors found!</p>
+								)}
+								{trinka.response.map((error, index) => (
+									<div
+										key={index}
+										className={
+											index % 2 === 0 ? "even" : "odd"
+										}
+									>
 										<p>
 											<strong>
-												Suggested Corrections:{" "}
+												Sentence {index + 1}:{" "}
 											</strong>
+											{error.sentence}
 										</p>
-										<ol>
-											{error.better.map(
-												(correction, idx) => (
-													<li key={idx}>
-														{correction}
-													</li>
-												)
-											)}
-										</ol>
+										{error.sentence_result.map(
+											(result, idx) => (
+												<div
+													key={idx}
+													style={{
+														paddingLeft: "15px",
+													}}
+												>
+													<p>
+														<strong>
+															Wrong Part {idx + 1}
+															:{" "}
+														</strong>
+														{result.covered_text}
+													</p>
+													{result.output.map(
+														(output, i) => (
+															<div
+																key={i}
+																style={{
+																	paddingLeft:
+																		"15px",
+																}}
+															>
+																<p>
+																	<strong>
+																		Suggested
+																		Corrections{" "}
+																		{i + 1}:{" "}
+																	</strong>
+																	{
+																		output.revised_text
+																	}
+																</p>
+																<p>
+																	<strong>
+																		Comment:{" "}
+																	</strong>
+																	{
+																		output.comment
+																	}
+																</p>
+															</div>
+														)
+													)}
+												</div>
+											)
+										)}
 									</div>
-									<p>
-										<strong>Explanation: </strong>
-										{error.description.en}
-									</p>
-								</div>
-							))}
+								))}
+							</div>
 						</div>
-					</div>
-				)}
-				{languagetool?.matches && (
-					<div>
-						<h2>Results (LanguageTool API)</h2>
-						<div className="errors">
-							{languagetool.matches.length === 0 && (
-								<p>No errors found!</p>
-							)}
-							{languagetool.matches.map((error, index) => (
-								<div
-									key={index}
-									className={index % 2 === 0 ? "even" : "odd"}
-								>
-									{/* <p>
-										<strong>Error {index + 1}: </strong>
-										{error.bad}
-									</p> */}
-									<div>
-										<p>
-											<strong>
-												Suggested Corrections:{" "}
-											</strong>
-										</p>
-										<ol>
-											{error.replacements.map(
-												(correction, idx) => (
-													<li key={idx}>
-														{correction.value}
-													</li>
-												)
-											)}
-										</ol>
-									</div>
-									<p>
-										<strong>Explanation: </strong>
-										{error.message}
-									</p>
-								</div>
-							))}
-						</div>
-					</div>
-				)}
-				{gramformer[0].edits.length > 0 && (
-					<div>
-						<h2>Results (Gramformer API)</h2>
-						<div className="errors">
-							{gramformer[0].edits.length === 0 && (
-								<p>No errors found!</p>
-							)}
-							{gramformer[0].edits.length && (
-								<div>
-									<p>
-										<strong>Corrected Text: </strong>
-										{gramformer[0].corrected}
-									</p>
-									{gramformer[0].edits.map((edit, idx) => (
+					)}
+					{textgears?.status && (
+						<div>
+							<h2>Results (TextGears API)</h2>
+							<div className="errors">
+								{textgears.response.errors.length === 0 && (
+									<p>No errors found!</p>
+								)}
+								{textgears.response.errors.map(
+									(error, index) => (
 										<div
-											key={idx}
+											key={error.id}
 											className={
-												idx % 2 === 0 ? "even" : "odd"
+												index % 2 === 0 ? "even" : "odd"
 											}
 										>
 											<p>
-												<strong>Error Type: </strong>
-												{edit[0]}
+												<strong>
+													Error {index + 1}:{" "}
+												</strong>
+												{error.bad}
 											</p>
+											<div>
+												<p>
+													<strong>
+														Suggested Corrections:{" "}
+													</strong>
+												</p>
+												<ol>
+													{error.better.map(
+														(correction, idx) => (
+															<li key={idx}>
+																{correction}
+															</li>
+														)
+													)}
+												</ol>
+											</div>
 											<p>
-												<strong>Error: </strong>
-												{edit[1]}{" "}
-												<strong>&nbsp; At: </strong>
-												{edit[2] +
-													", " +
-													edit[3] +
-													" word position"}
-											</p>
-											<p>
-												<strong>Correction: </strong>
-												{edit[4]}{" "}
-												<strong>&nbsp; At: </strong>
-												{edit[5] +
-													", " +
-													edit[6] +
-													" word position"}
+												<strong>Explanation: </strong>
+												{error.description.en}
 											</p>
 										</div>
-									))}
-								</div>
-							)}
+									)
+								)}
+							</div>
 						</div>
-					</div>
-				)}
-			</div>
+					)}
+					{languagetool?.matches && (
+						<div>
+							<h2>Results (LanguageTool API)</h2>
+							<div className="errors">
+								{languagetool.matches.length === 0 && (
+									<p>No errors found!</p>
+								)}
+								{languagetool.matches.map((error, index) => (
+									<div
+										key={index}
+										className={
+											index % 2 === 0 ? "even" : "odd"
+										}
+									>
+										{/* <p>
+										<strong>Error {index + 1}: </strong>
+										{error.bad}
+									</p> */}
+										<div>
+											<p>
+												<strong>
+													Suggested Corrections:{" "}
+												</strong>
+											</p>
+											<ol>
+												{error.replacements.map(
+													(correction, idx) => (
+														<li key={idx}>
+															{correction.value}
+														</li>
+													)
+												)}
+											</ol>
+										</div>
+										<p>
+											<strong>Explanation: </strong>
+											{error.message}
+										</p>
+									</div>
+								))}
+							</div>
+						</div>
+					)}
+					{gramformer[0] && (
+						<div>
+							<h2>Results (Gramformer API)</h2>
+							<div className="errors">
+								{gramformer[0]?.edits.length === 0 && (
+									<p>No errors found!</p>
+								)}
+								{gramformer[0]?.edits.length && (
+									<div>
+										<p>
+											<strong>Corrected Text: </strong>
+											{gramformer[0].corrected}
+										</p>
+										{gramformer[0].edits.map(
+											(edit, idx) => (
+												<div
+													key={idx}
+													className={
+														idx % 2 === 0
+															? "even"
+															: "odd"
+													}
+												>
+													<p>
+														<strong>
+															Error Type:{" "}
+														</strong>
+														{edit[0]}
+													</p>
+													<p>
+														<strong>Error: </strong>
+														{edit[1]}{" "}
+														<strong>
+															&nbsp; At:{" "}
+														</strong>
+														{edit[2] +
+															", " +
+															edit[3] +
+															" word position"}
+													</p>
+													<p>
+														<strong>
+															Correction:{" "}
+														</strong>
+														{edit[4]}{" "}
+														<strong>
+															&nbsp; At:{" "}
+														</strong>
+														{edit[5] +
+															", " +
+															edit[6] +
+															" word position"}
+													</p>
+												</div>
+											)
+										)}
+									</div>
+								)}
+							</div>
+						</div>
+					)}
+				</div>
+			)}
 		</>
 	);
 }
